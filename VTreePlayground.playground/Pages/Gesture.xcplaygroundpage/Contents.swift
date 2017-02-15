e1250320d7447c56ef04c931606b0bc82cf81db3
Yasuhiro Inami <inamiy@gmail.com>
2017-02-15 23:49:57 +0900

    [Playground] Add gesture dragging demo

--- VTreePlayground.playground/Pages/Gesture.xcplaygroundpage/Contents.swift
+++ VTreePlayground.playground/Pages/Gesture.xcplaygroundpage/Contents.swift
@@ -7,6 +7,32 @@ struct Model
 {
     let rootSize = CGSize(width: 320, height: 480)
     let message: String
+    let cursor: Cursor?
+
+    struct Cursor
+    {
+        let frame: CGRect
+        let backgroundColor: UIColor
+    }
+}
+
+extension Model.Cursor
+{
+    init?(msg: Msg)
+    {
+        switch msg {
+            case let .tap(context):
+                let frame = CGRect(origin: context.location, size: .zero)
+                    .insetBy(dx: -20, dy: -20)
+                self = Model.Cursor(frame: frame, backgroundColor: .orange)
+            case let .pan(context) where context.state == .changed:
+                let frame = CGRect(origin: context.location, size: .zero)
+                    .insetBy(dx: -20, dy: -20)
+                self = Model.Cursor(frame: frame, backgroundColor: .green)
+            default:
+                return nil
+        }
+    }
 }
 
 func update(_ model: Model, _ msg: Msg) -> Model
@@ -14,7 +40,12 @@ func update(_ model: Model, _ msg: Msg) -> Model
     print(msg)  // Warning: impure logging
 
     let argsString = msg.rawValue.arguments.map { "\($0)" }.joined(separator: "\n")
-    return Model(message: "\(msg.rawValue.funcName)\n\(argsString)")
+    let cursor = Model.Cursor(msg: msg)
+
+    return Model(
+        message: "\(msg.rawValue.funcName)\n\(argsString)",
+        cursor: cursor
+    )
 }
 
 func view(model: Model) -> VView<Msg>
@@ -22,13 +53,13 @@ func view(model: Model) -> VView<Msg>
     let rootWidth = model.rootSize.width
     let rootHeight = model.rootSize.height
 
-    func rootView(_ children: [AnyVTree<Msg>]) -> VView<Msg>
+    func rootView(_ children: [AnyVTree<Msg>?]) -> VView<Msg>
     {
         return VView(
             frame: CGRect(x: 0, y: 0, width: rootWidth, height: rootHeight),
             backgroundColor: .white,
             gestures: [.tap(^Msg.tap), .pan(^Msg.pan), .longPress(^Msg.longPress), .swipe(^Msg.swipe), .pinch(^Msg.pinch), .rotation(^Msg.rotation)],
-            children: children
+            children: children.flatMap { $0 }
         )
     }
 
@@ -54,13 +85,23 @@ func view(model: Model) -> VView<Msg>
         )
     }
 
+    func cursorView(_ cursor: Model.Cursor) -> VView<Msg>
+    {
+        return VView(
+            key: key("cursor"),
+            frame: cursor.frame,
+            backgroundColor: cursor.backgroundColor
+        )
+    }
+
     return rootView([
         *label(model.message),
-        *noteLabel()
+        *noteLabel(),
+        model.cursor.map(cursorView).map(*)
     ])
 }
 
-let model = Model(message: "Initial")
+let model = Model(message: "Initial", cursor: nil)
 
 let program = Program(model: model, update: update, view: view, debug: false)
 
