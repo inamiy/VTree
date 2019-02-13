5cc003e85e531b85f28b1f9d9bfc893dd4b85ec9
Yasuhiro Inami <inamiy@gmail.com>
2019-02-13 15:03:47 +0900

    [Test] Update generated code

--- Tests/Fixtures/CodeGenerated/Message.generated.swift
+++ Tests/Fixtures/CodeGenerated/Message.generated.swift
@@ -1,14 +1,12 @@
-// Generated using Sourcery 0.5.3 — https://github.com/krzysztofzablocki/Sourcery
+// Generated using Sourcery 0.15.0 — https://github.com/krzysztofzablocki/Sourcery
 // DO NOT EDIT
 
 import VTree
-
 extension MyGestureMsg: Message
 {
     public init?(rawMessage: RawMessage)
     {
         switch rawMessage.funcName {
-
             // .msg1(GestureContext)
             case "msg1":
                 let arguments = rawMessage.arguments
@@ -18,7 +16,6 @@ extension MyGestureMsg: Message
                 else {
                     return nil
                 }
-
             // .msg2(GestureContext)
             case "msg2":
                 let arguments = rawMessage.arguments
@@ -28,7 +25,6 @@ extension MyGestureMsg: Message
                 else {
                     return nil
                 }
-
             // .msg3(GestureContext)
             case "msg3":
                 let arguments = rawMessage.arguments
@@ -38,7 +34,6 @@ extension MyGestureMsg: Message
                 else {
                     return nil
                 }
-
             // .msg4(GestureContext)
             case "msg4":
                 let arguments = rawMessage.arguments
@@ -48,7 +43,6 @@ extension MyGestureMsg: Message
                 else {
                     return nil
                 }
-
             default:
                 return nil
         }
@@ -57,29 +51,22 @@ extension MyGestureMsg: Message
     public var rawMessage: RawMessage
     {
         switch self {
-
             case let .msg1(context):
                 return RawMessage(funcName: "msg1", arguments: context.rawArguments)
-
             case let .msg2(context):
                 return RawMessage(funcName: "msg2", arguments: context.rawArguments)
-
             case let .msg3(context):
                 return RawMessage(funcName: "msg3", arguments: context.rawArguments)
-
             case let .msg4(context):
                 return RawMessage(funcName: "msg4", arguments: context.rawArguments)
-
         }
     }
 }
-
 extension MyGestureMsg2: Message
 {
     public init?(rawMessage: RawMessage)
     {
         switch rawMessage.funcName {
-
             // .msg1(GestureContext)
             case "msg1":
                 let arguments = rawMessage.arguments
@@ -89,7 +76,6 @@ extension MyGestureMsg2: Message
                 else {
                     return nil
                 }
-
             // .msg2(GestureContext)
             case "msg2":
                 let arguments = rawMessage.arguments
@@ -99,7 +85,6 @@ extension MyGestureMsg2: Message
                 else {
                     return nil
                 }
-
             // .msg3(GestureContext)
             case "msg3":
                 let arguments = rawMessage.arguments
@@ -109,7 +94,6 @@ extension MyGestureMsg2: Message
                 else {
                     return nil
                 }
-
             // .msg4(GestureContext)
             case "msg4":
                 let arguments = rawMessage.arguments
@@ -119,7 +103,6 @@ extension MyGestureMsg2: Message
                 else {
                     return nil
                 }
-
             default:
                 return nil
         }
@@ -128,41 +111,30 @@ extension MyGestureMsg2: Message
     public var rawMessage: RawMessage
     {
         switch self {
-
             case let .msg1(context):
                 return RawMessage(funcName: "msg1", arguments: context.rawArguments)
-
             case let .msg2(context):
                 return RawMessage(funcName: "msg2", arguments: context.rawArguments)
-
             case let .msg3(context):
                 return RawMessage(funcName: "msg3", arguments: context.rawArguments)
-
             case let .msg4(context):
                 return RawMessage(funcName: "msg4", arguments: context.rawArguments)
-
         }
     }
 }
-
 extension MyMsg: Message
 {
     public init?(rawMessage: RawMessage)
     {
         switch rawMessage.funcName {
-
             case "msg1":
                 self = .msg1
-
             case "msg2":
                 self = .msg2
-
             case "msg3":
                 self = .msg3
-
             case "msg4":
                 self = .msg4
-
             default:
                 return nil
         }
@@ -171,35 +143,26 @@ extension MyMsg: Message
     public var rawMessage: RawMessage
     {
         switch self {
-
             case .msg1:
                 return RawMessage(funcName: "msg1", arguments: [])
-
             case .msg2:
                 return RawMessage(funcName: "msg2", arguments: [])
-
             case .msg3:
                 return RawMessage(funcName: "msg3", arguments: [])
-
             case .msg4:
                 return RawMessage(funcName: "msg4", arguments: [])
-
         }
     }
 }
-
 extension MyMsg2: Message
 {
     public init?(rawMessage: RawMessage)
     {
         switch rawMessage.funcName {
-
             case "test1":
                 self = .test1
-
             case "test2":
                 self = .test2
-
             default:
                 return nil
         }
@@ -208,14 +171,10 @@ extension MyMsg2: Message
     public var rawMessage: RawMessage
     {
         switch self {
-
             case .test1:
                 return RawMessage(funcName: "test1", arguments: [])
-
             case .test2:
                 return RawMessage(funcName: "test2", arguments: [])
-
         }
     }
 }
-
